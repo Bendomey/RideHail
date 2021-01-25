c7b428ac45a64f68fbfd4f4aee2a0fd74e4208cc
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-25 16:18:00 +0000

    modelled customer

---
+++ account/internal/orm/models/customer.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+// Customer defines a customer who requests for a ride hail service
+type Customer struct {
+	BaseModelSoftDelete
+	LastName   string `gorm:"not null;"`
+	OtherNames *string
+	Email      *string
+	Phone      string `gorm:"not null;"`
+}
+
+// BeforeSave hook is called before the data is persisted to db
+func (customer *Customer) BeforeSave(tx *gorm.DB) (err error) {
+
+	//remove 0 from phone and adds 233
+	trimmedPhone := strings.TrimSpace(customer.Phone)
+	ghCodeAdded := "233" + trimmedPhone[1:10]
+	customer.Phone = ghCodeAdded
+
+	return
+}
