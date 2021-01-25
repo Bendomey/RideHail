941c977244cc6965f454ae145140a1d2c045d188
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-25 17:46:39 +0000

    added admin service

--- account/cmd/main.go
+++ account/cmd/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	log "github.com/Bendomey/RideHail/account/internal/logger"
 	"github.com/Bendomey/RideHail/account/internal/orm"
+	"github.com/Bendomey/RideHail/account/internal/services"
 )
 
 func main() {
 	// creates a new ORM instance to send it to our server
-	if _, err := orm.Factory(); err != nil {
+	orm, err := orm.Factory()
+	if err != nil {
 		log.Panic(err)
 	}
+
+	//call service
+	services.NewAdminSvc(orm)
 
 	log.NewLogger().Print("Hello world")
 }
