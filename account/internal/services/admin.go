3fb956d559b1b3de24b2045bec1b250db4e2c794
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-26 04:21:43 +0000

    injected redis config and working on forgot password

--- account/internal/services/admin.go
+++ account/internal/services/admin.go
@@ -3,13 +3,17 @@ package services
 import (
 	"context"
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/Bendomey/RideHail/account/internal/orm"
 	"github.com/Bendomey/RideHail/account/internal/orm/models"
 	"github.com/Bendomey/RideHail/account/pkg/utils"
+	"github.com/Bendomey/goutilities/pkg/generatecode"
 	"github.com/Bendomey/goutilities/pkg/signjwt"
 	"github.com/Bendomey/goutilities/pkg/validatehash"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
 )
 
@@ -20,11 +24,13 @@ type AdminService interface {
 	UpdateAdminRole(ctx context.Context, adminID string, role string) (bool, error)
 	UpdateAdmin(ctx context.Context, adminID string, fullname *string, email *string, phone *string) (bool, error)
 	UpdateAdminPassword(ctx context.Context, adminID string, oldPassword string, newPassword string) (bool, error)
+	DeleteAdmin(ctx context.Context, adminID string) (bool, error)
 }
 
 //ORM gets orm connection
 type ORM struct {
-	DB *orm.ORM
+	DB  *orm.ORM
+	rdb *redis.Client
 }
 
 //LoginResult is the typing for returning login successful data to user
@@ -34,8 +40,8 @@ type LoginResult struct {
 }
 
 // NewAdminSvc exposed the ORM to the admin functions in the module
-func NewAdminSvc(db *orm.ORM) AdminService {
-	return &ORM{db}
+func NewAdminSvc(db *orm.ORM, rdb *redis.Client) AdminService {
+	return &ORM{db, rdb}
 }
 
 // CreateAdmin creates an admin when invoked
@@ -142,4 +148,105 @@ func (orm *ORM) UpdateAdminPassword(ctx context.Context, adminID string, oldPass
 	orm.DB.DB.Save(&_Admin)
 	return true, nil
 
+}
+
+// DeleteAdmin deletes an admin
+func (orm *ORM) DeleteAdmin(ctx context.Context, adminID string) (bool, error) {
+	var _Admin models.Admin
+
+	err := orm.DB.DB.Delete(&_Admin, "id = ?", adminID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, errors.New("AdminNotFound")
+	}
+	return true, nil
+}
+
+/**
+* Forgot Password Fucntionality
+* 1. Request with Email
+* 2. Send Code And save code in redis
+* 3. Compare code And send response
+* 4. Enter new password and remove from redis
+ */
+
+// ForgotPasswordRequest is to start the process
+func (orm *ORM) ForgotPasswordRequest(ctx context.Context, email string) (*models.Admin, error) {
+	var _Admin models.Admin
+
+	// check if admin exists
+	err := orm.DB.DB.First(&_Admin, "email = ?", email).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("AdminNotFound")
+	}
+
+	//generate code
+	code := generatecode.GenerateCode(6)
+
+	// send code to email
+
+	//save in redis and expire in an hours time
+	redisErr := orm.rdb.Set(ctx, fmt.Sprintf("%s", _Admin.ID), code, 1*time.Hour).Err()
+	if redisErr != nil {
+		return nil, redisErr
+	}
+
+	return &_Admin, nil
+}
+
+//ResendCode helps to resend a new code
+func (orm *ORM) ResendCode(ctx context.Context, adminID string) (*models.Admin, error) {
+	var _Admin models.Admin
+
+	// check if admin exists
+	err := orm.DB.DB.First(&_Admin, "id = ?", adminID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("AdminNotFound")
+	}
+
+	//generate code
+	code := generatecode.GenerateCode(6)
+
+	// send code to email
+
+	//save in redis and expire in an hours time
+	redisErr := orm.rdb.Set(ctx, fmt.Sprintf("%s", _Admin.ID), code, 1*time.Hour).Err()
+	if redisErr != nil {
+		return nil, redisErr
+	}
+
+	return &_Admin, nil
+}
+
+// CompareAdminCodes compares the admin code sent by user
+func (orm *ORM) CompareAdminCodes(ctx context.Context, adminID string, code string) (bool, error) {
+	//check in redis to see if its the same and not expired
+	value, err := orm.rdb.Get(ctx, fmt.Sprintf("%s", adminID)).Result()
+	if err == redis.Nil {
+		return false, errors.New("CodeHasExpired")
+	} else if err != nil {
+		return false, err
+	}
+
+	if value != code {
+		return false, errors.New("CodeIncorrect")
+	}
+	return true, nil
+}
+
+// ResetPassword updates the admins new password
+func (orm *ORM) ResetPassword(ctx context.Context, adminID string, password string) (bool, error) {
+	var _Admin models.Admin
+
+	err := orm.DB.DB.First(&_Admin, "id = ?", adminID).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, errors.New("AdminNotFound")
+	}
+
+	//update password
+	_Admin.Password = password
+	orm.DB.DB.Save(&_Admin)
+
+	//invalidate the redis data pertaining to this admin
+
+	return true, nil
 }
