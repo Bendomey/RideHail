1eb971415779b9b0f0748e658ac78b8c4c253c96
Benjamin Domey <domeybenjamin1@gmail.com>
2021-01-26 04:20:54 +0000

    added redis configuration

--- account/internal/redis/main.go
+++ account/internal/redis/main.go
@@ -1,1 +1,28 @@
 package redis
+
+import (
+	"fmt"
+
+	"github.com/Bendomey/RideHail/account/pkg/utils"
+	"github.com/go-redis/redis/v8"
+)
+
+var host, port, password string
+
+func init() {
+	host = utils.MustGet("REDIS_HOST")
+	port = utils.MustGet("REDIS_PORT")
+	password = ""
+}
+
+// Factory connects app to reid
+func Factory() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		Password: password, // no password set
+		DB:       0,        // use default DB
+	})
+
+	return rdb
+
+}
